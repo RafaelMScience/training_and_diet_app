60b6c02c7eaa9770764ebdca650731c732558c60
RafaelM <RafaelM.Science@gmail.com>
2020-04-18 19:48:16 -0400

    Feat: Card Meals
    Foi adicionado os card Meals For Today

---
+++ lib/ui/pages/profile_screen.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:traininganddietapp/model/meal.dart';
+
+class ProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: const Color(0xFFE9E9E9),
+      bottomNavigationBar: ClipRRect(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(40)),
+        child: BottomNavigationBar(
+          iconSize: 40,
+          selectedIconTheme: IconThemeData(
+            color: const Color(0xFF200087),
+          ),
+          unselectedIconTheme: IconThemeData(
+            color: Colors.black12,
+          ),
+          items: [
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Icon(Icons.home),
+              ),
+              title: Text(
+                "Home",
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Icon(Icons.search),
+              ),
+              title: Text(
+                "Search",
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Icon(Icons.person),
+              ),
+              title: Text(
+                "Profile",
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            height: height * 0.35,
+            left: 0,
+            right: 0,
+            child: ClipRRect(
+              borderRadius: const BorderRadius.vertical(
+                bottom: const Radius.circular(40),
+              ),
+              child: Container(
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Positioned(
+            top: height * 0.38,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: height * 0.6,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      bottom: 8,
+                      left: 32,
+                      right: 16,
+                    ),
+                    child: Text(
+                      "MEALS FOR TODAY",
+                      style: const TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: <Widget>[
+                          SizedBox(
+                            width: 32,
+                          ),
+                          for (int i = 0; i < meals.length; i++)
+                            _MealCard(meal: meals[i]),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Expanded(
+                    child: Container(
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _MealCard extends StatelessWidget {
+  final Meal meal;
+
+  const _MealCard({Key key, @required this.meal}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(
+        right: 20,
+        bottom: 10,
+      ),
+      child: Material(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        elevation: 4,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            Flexible(
+              fit: FlexFit.tight,
+              child: ClipRRect(
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+                child: Image.asset(
+                  meal.imagePath,
+                  width: 150,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Flexible(
+              fit: FlexFit.tight,
+              child: Column(
+                children: <Widget>[
+                  Text(meal.mealTime),
+                  Text(meal.name),
+                  Text(meal.kiloCaloriesBurnt),
+                  Text(meal.timeTaken),
+                  SizedBox(height: 16),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
